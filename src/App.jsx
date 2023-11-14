bac622a9b5d48146ef3340536fb1af16b997f4c4
vincentmaestro <maestroobika@gmail.com>
2023-11-14 21:01:55 +0100

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Details from "./Details";
-import Home from "./Home";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Details from "./Details";
+import Home from "./Home";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [triggerSwitch, settriggerSwitch] = useState(false);
 
   async function getCountries() {
     const request = await fetch('https://restcountries.com/v3.1/all');
@@ -23,18 +23,18 @@ function App() {
   }
 
   function switchTheme() {
-    darkMode == false ? setLight() : setDark();
+    triggerSwitch == false ? setLight() : setDark();
   }
 
   useEffect(() => {
     switchTheme();
-  }, [darkMode]);
+  }, [triggerSwitch]);
 
   return (
     <Router>
       <div className="toggle-mode-nav flex justify-between py-3 mb-8 px-10 sticky top-0 375:px-4">
         <h1 className="font-bold">Where in the world?</h1>
-        <p className="flex items-center gap-[5px] cursor-pointer font-medium" onClick={() => {setDarkMode(!darkMode)}}><i className="ri-moon-line"></i>Dark Mode</p>
+        <p className="flex items-center gap-[5px] cursor-pointer font-medium" onClick={() => {settriggerSwitch(!triggerSwitch)}}><i className="ri-moon-line"></i>Dark Mode</p>
       </div>
       <Switch>
         <Route exact path="/">
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,46 +1,23 @@
 import { useState, useEffect } from "react";
-import AllCountries from "./AllCountries";
-import Africa from "./Africa";
-import America from "./America";
-import Asia from "./Asia";
-import Europe from "./Europe";
-import Oceania from "./Oceania";
+import { Link } from "react-router-dom";
 
 function Home({countries}) {
     const [allCountries, setAllCountries] = useState(null);
-    const [africa, setAfrica] = useState(null);
-    const [america, setAmerica] = useState(null);
-    const [asia, setAsia] = useState(null);
-    const [europe, setEurope] = useState(null);
-    const [oceania, setOceania] = useState(null);
-
     const [searchText, setSearchText] = useState('');
-    const [region, setRegion] = useState(sessionStorage.getItem('region'));
-            
-    //const [filterWorld, setFilterWorld] = useState(true);
-    const [filterAfrica, setfilterAfrica] = useState(false);
-    const [filterAmerica, setfilterAmerica] = useState(false);
-    const [filterAsia, setfilterAsia] = useState(false);
-    const [filterEurope, setfilterEurope] = useState(false);
-    const [filterOceania, setfilterOceania] = useState(false);
-    
-    async function getCountries() {
-        const data = await countries();
-        // setAllCountries(data);
-        setAfrica(data.filter(region => region.region == 'Africa'));
-        setAmerica(data.filter(region => region.region == 'Americas'));
-        setAsia(data.filter(region => region.region == 'Asia'));
-        setEurope(data.filter(region => region.region == 'Europe'));
-        setOceania(data.filter(region => region.region == 'Oceania'));
-        
-        if (region == null) setAllCountries(data);
-        else if (region == 'allCountries') setAllCountries(data);
-        else setAllCountries(null);
-        region == 'Africa' ? setfilterAfrica(true) : setfilterAfrica(false);
-        region == 'America' ? setfilterAmerica(true) : setfilterAmerica(false);
-        region == 'Asia' ? setfilterAsia(true) : setfilterAsia(false);
-        region == 'Europe' ? setfilterEurope(true) : setfilterEurope(false);
-        region == 'Oceania' ? setfilterOceania(true) : setfilterOceania(false);
+    const [region, setregion] = useState('Filter by region');
+    const regions = [ 'Filter by region', 'Africa', 'America', 'Asia', 'Europe', 'Oceania' ];
+    const [showregions, setShowregions] = useState(false);
+
+    async function getCountries () {
+        if(region === 'Filter by region') {
+            const data = await countries();
+            setAllCountries(data);
+        }
+        else {
+            const request = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+            const data = await request.json();
+            setAllCountries(data);
+        }
     }
 
     useEffect(() => {
@@ -51,25 +28,44 @@ function Home({countries}) {
         <div className="appwrapper">
             <div className="filter-countries flex justify-between px-14 mb-[50px] 576:px-4 430:flex-col 430:gap-[30px]">
                 <div className="left">
-                    <i className="ri-search-line relative left-8 font-medium 430:top-8"></i><input type="text" name="search countries" className="py-[5px] pl-[50px] w-[300px] outline-none" placeholder="Search for a contry..." value={searchText} onChange={e => {setSearchText(e.target.value);}} />
+                    <i className="ri-search-line relative left-8 font-medium 430:top-8 430:left-3"></i>
+                    <input type="text" name="search countries" className="py-[5px] pl-[50px] w-[300px] outline-none 430:pl-[40px]" placeholder="Search for a country..." value={searchText} onChange={e => {setSearchText(e.target.value);}} />
                 </div>
-                <div className="right">
-                    <select className="select-country outline-none p-[7px] rounded-[5px]" value={region} onChange={e => {sessionStorage.setItem('region', e.target.value); setRegion(sessionStorage.getItem('region'));}} >
-                        <option value="allCountries">Filter by Region</option>
-                        <option value="Africa">Africa</option>
-                        <option value="America">America</option>
-                        <option value="Asia">Asia</option>
-                        <option value="Europe">Europe</option>
-                        <option value="Oceania">Oceania/Australia</option>
-                    </select>
+                <div className="flex w-[180px] select-none border-2 border-gray-300 relative" onClick={() => setShowregions(!showregions)}>
+                    <div className="flex items-center justify-between px-2 w-full">
+                        <span className="font-semibold">{region}</span>
+                        <i class="ri-arrow-drop-down-line text-lg font-semibold"></i>
+                    </div>
+                    {showregions &&
+                        <ul className="absolute top-[40px] bg-slate-500 p-2 w-full">
+                            {regions.map((region, key) => (
+                                <li className="hover:text-slate-200 cursor-pointer" key={key} onClick={() => setregion(region)}>{region}</li>
+                            ))}
+                        </ul>
+                    }
                 </div>
             </div>
-            {allCountries && <AllCountries world = {allCountries} searchText = {searchText} /> }
-            {filterAfrica && <Africa AfricanCountries = {africa} searchText = {searchText} /> }
-            {filterAmerica && <America AmericanCountries = {america} searchText = {searchText} />}
-            {filterAsia && <Asia AsianCountries = {asia} searchText = {searchText} />}
-            {filterEurope && <Europe EuropeanCountries = {europe} searchText = {searchText} />}
-            {filterOceania && <Oceania OceaniaCountries = {oceania} searchText = {searchText} />}
+            {!allCountries && <p className="fixed top-[50%] left-[50%]" style={{transform:"translate(-50%, -50%)"}}>Loading...</p> }
+            <ul className="country-list flex flex-wrap gap-[50px] pl-14 pb-14 576:p-0">
+                {allCountries && allCountries.filter(country => {
+                    if(!searchText.length) return country
+                    if(country.name.common.toLowerCase().includes(searchText.toLowerCase())) return country
+                }).map((country, index) => (
+                <li key={index} className="country w-[210px] rounded-tl-[5px] rounded-tr-[5px] pb-[50px] overflow-hidden">
+                    <Link to={`/details/${country.name.common}`}>
+                        <div className="flag">
+                            <img src={country.flags.svg} alt={country.flags.alt} className="w-[100%] h-[120px]" />
+                        </div>
+                        <div className="details pl-4">
+                            <h2 className="name my-[10px] font-bold">{country.name.common}</h2>
+                            <p className="population"><span className="font-semibold">Population: </span>{country.population}</p>
+                            <p className="region"><span className="font-semibold">Region: </span> {country.region}</p>
+                            <p className="capital"><span className="font-semibold">Capital: </span>{country.capital}</p>
+                        </div>
+                    </Link>
+                </li>
+                ))}
+            </ul>
         </div>
     );
 }
